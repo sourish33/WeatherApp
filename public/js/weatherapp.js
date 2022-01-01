535e2de517574bea1bee765a5a78f2a627a61ac8
SD <sourish.d@gmail.com>
2022-01-01 12:11:45 -0600

    get weather button can return input to console

--- public/js/weatherapp.js
+++ public/js/weatherapp.js
@@ -1,1 +1,11 @@
 console.log("weatherapp.js speaking")
+
+const button = document.getElementById("submitBtn")
+const formInput = document.getElementById("formInput")
+
+const handleClick= () =>{
+    console.log(`You entered ${formInput.value}`)
+}
+
+
+button.addEventListener("click", handleClick)
